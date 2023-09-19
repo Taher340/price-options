ac626726cb1152b4e52d798e8e5ac00b1b1dd932
Taher340 <ahmedabutaher@gmail.com>
2023-09-19 11:00:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-4xl bg-red-200'>Vite + React</h1>
+
+      c
+    </>
+  )
+}
+
+export default App
